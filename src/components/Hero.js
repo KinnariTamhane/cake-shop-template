502d3c690f9203111505ba1b124be680e21f8d23
Kinnari Tamhane <kinnaritamhane@Kinnari-Tamhane.local>
2026-01-28 15:21:38 +0530

    improving performance

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import './Hero.css';
 const Hero = () => {
   return (
     <section id="home" className="hero">
-      <div className="hero-background">
+      <div className="hero-background" fetchpriority="high">
         <h2 className="hero-tagline">Crave. Customize. Collect.</h2>
       </div>
     </section>
